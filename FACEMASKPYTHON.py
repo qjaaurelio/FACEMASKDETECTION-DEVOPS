b2c51415f8aab9ce6126a80e8341b1b3fc29bfee
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 05:10:57 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -49,15 +49,7 @@ class VideoTransformer(VideoTransformerBase):
         frm = frame.to_ndarray(format="bgr24")
 
         faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-        predictions = model.predict(frm)
-
-        if predictions[0][0] < 0.5:
-            label = "With Mask"
-        else:
-            label = "Without Mask"
-
-        # Overlay the label on the frame
-        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        
 
         for x, y, w, h in faces:
             cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
