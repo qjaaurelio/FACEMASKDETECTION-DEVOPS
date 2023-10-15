ac4ec5fb30f953d44dfcd5d277d16a299880dce9
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 05:37:42 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from PIL import Image
 import numpy as np
 import tensorflow as tf
 import cv2
@@ -30,22 +31,29 @@ def preprocess_frame(frame):
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-        frm = frame.to_ndarray(format="bgr24")
-
-        # Face detection
-        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-        for x, y, w, h in faces:
-            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
         preprocessed_frame = preprocess_frame(frame)
         predictions = model.predict(preprocessed_frame)
+
         if predictions[0][0] < 0.5:
             label = "With Mask"
         else:
             label = "Without Mask"
-        cv2.putText(frm, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        # Overlay the label on the frame
+        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        return frame
+
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+
+        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+        for x, y, w, h in faces:
+            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
         return av.VideoFrame.from_ndarray(frm, format='bgr24')
+
 
 webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoTransformer,
                 rtc_configuration=RTCConfiguration(
