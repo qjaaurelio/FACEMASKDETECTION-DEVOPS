611be51d9ec60a745a696baaf21edf67fa7886d6
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 03:41:24 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -1,16 +1,17 @@
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-from PIL import Image, ImageOps
+from PIL import Image
 import numpy as np
 import tensorflow as tf
 import cv2
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('FacemaskModel')
-  return model
+    model = tf.keras.models.load_model('FacemaskModel')
+    return model
 
 model = load_model()
+
 st.write("""
 # Mask Detection System
 """)
@@ -23,32 +24,20 @@ def preprocess_frame(frame):
     frame = np.expand_dims(frame, axis=0)  # Add batch dimension
     return frame
 
-# Capture video from the webcam
-cap = cv2.VideoCapture(0)  # 0 for the default camera
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        preprocessed_frame = preprocess_frame(frame)
+        predictions = model.predict(preprocessed_frame)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+        if predictions[0][0] < 0.5:
+            label = "With Mask"
+        else:
+            label = "Without Mask"
 
-    preprocessed_frame = preprocess_frame(frame)
+        # Overlay the label on the frame
+        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    # Run inference with preprocessed_frame
-    predictions = model.predict(preprocessed_frame)
+        return frame
 
-    # Post-process the predictions
-    if predictions[0][0] < 0.5:  # You can adjust this threshold
-        label = "With Mask"
-    else:
-        label = "Without Mask"
-
-    # Overlay the label on the frame
-    cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    # Display the frame with the result
-    cv2.imshow('Face Mask Detection', frame)
-
-    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
-        break
-
-cap.release()
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
