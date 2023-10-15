3827b76699b59db00f69a954a1b39ec1b7b9089c
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 04:20:18 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -52,20 +52,6 @@ class VideoTransformer(VideoTransformerBase):
         for x, y, w, h in faces:
             cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-        preprocessed_frame = preprocess_frame(frame)
-        predictions = model.predict(preprocessed_frame)
-
-        if predictions[0][0] < 0.5:
-            label = "With Mask"
-        else:
-            label = "Without Mask"
-
-        # Overlay the label on the frame
-        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-        return frame
-
 
         return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
