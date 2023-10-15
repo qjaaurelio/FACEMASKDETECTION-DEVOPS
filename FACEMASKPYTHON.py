7398c5f7bc6f85de8e785d3a2f2c288eb6a18ec0
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 03:51:54 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -5,7 +5,7 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('FacemaskModel')
     return model
