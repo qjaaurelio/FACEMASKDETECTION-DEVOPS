9c245454e2388373463a39d4dfb9335c9eb0682d
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 04:28:23 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -49,11 +49,25 @@ class VideoTransformer(VideoTransformerBase):
 
         faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
+        preprocessed_frame = preprocess_frame(frm)
+        predictions = model.predict(preprocessed_frm)
+
+        if predictions[0][0] < 0.5:
+            label = "With Mask"
+        else:
+            label = "Without Mask"
+
+        # Overlay the label on the frame
+        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        
+
         for x, y, w, h in faces:
             cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
 
         return av.VideoFrame.from_ndarray(frm, format='bgr24')
+        return frame
 
 
 webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoTransformer,
