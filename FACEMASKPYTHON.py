f2aac9d6285033d79e5e7294f80d7238612c5a11
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 09:01:28 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -28,13 +28,16 @@ def preprocess_frame(frame):
     frame = np.expand_dims(frame, axis=0)  # Add batch dimension
     return frame
 
-class VideoTransformer(VideoTransformerBase):
-    def recv(self, frame):
-        frm = frame.to_ndarray(format="bgr24")
+class VideoProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
 
-        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-        label = "With Mask"
-        cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+		for x,y,w,h in faces:
+			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+
+		return av.VideoFrame.from_ndarray(frm, format='bgr24')
         
         #predictions = model.predict(faces)
 
@@ -45,10 +48,6 @@ class VideoTransformer(VideoTransformerBase):
 
         # Overlay the label on the frame
         #cv2.putText(frm, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        for x, y, w, h in faces:
-            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
    # def recv(self, frame):
    #     frm = frame.to_ndarray(format="bgr24")
@@ -61,7 +60,7 @@ class VideoTransformer(VideoTransformerBase):
     #    return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
-webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoTransformer,
+webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
                 rtc_configuration=RTCConfiguration(
                     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
                 ))
