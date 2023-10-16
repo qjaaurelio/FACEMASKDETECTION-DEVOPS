32019c5ae8fa003ffbb9bacb0a49393527ca14b7
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 08:26:11 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -44,15 +44,15 @@ class VideoTransformer(VideoTransformerBase):
 
         return frame
 
-    def recv(self, frame):
-        frm = frame.to_ndarray(format="bgr24")
+   # def recv(self, frame):
+   #     frm = frame.to_ndarray(format="bgr24")
 
-        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+    #    faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
-        for x, y, w, h in faces:
-            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    #    for x, y, w, h in faces:
+    #        cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
+    #    return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
 webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoTransformer,
