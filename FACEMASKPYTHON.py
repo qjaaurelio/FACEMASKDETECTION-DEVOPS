5cecea11cac0bce1a8bc859f5948a73374242770
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 10:29:29 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -28,7 +28,45 @@ def preprocess_frame(frame):
     frame = np.expand_dims(frame, axis=0)  # Add batch dimension
     return frame
 
-class VideoProcessor:
+desired_size = (128, 128)  # Adjust to your model's input size
+
+def preprocess_frame(frame):
+    frame = cv2.resize(frame, desired_size)
+    frame = frame.astype(np.float32) / 255.0  # Normalize to [0, 1]
+    frame = np.expand_dims(frame, axis=0)  # Add batch dimension
+    return frame
+
+# Capture video from the webcam
+cap = cv2.VideoCapture(0)  # 0 for the default camera
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    preprocessed_frame = preprocess_frame(frame)
+
+    # Run inference with preprocessed_frame
+    predictions = model.predict(preprocessed_frame)
+
+    # Post-process the predictions
+    if predictions[0][0] < 0.5:  # You can adjust this threshold
+        label = "With Mask"
+    else:
+        label = "Without Mask"
+
+    # Overlay the label on the frame
+    cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    # Display the frame with the result
+    cv2.imshow('Face Mask Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+#class VideoProcessor:
 	#def recv(self, frame):
 		#frm = frame.to_ndarray(format="bgr24")
 	
@@ -42,20 +80,20 @@ class VideoProcessor:
 		#return av.VideoFrame.from_ndarray(frm, format='bgr24')
 		
 	
-	def transform(self, frame):
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-		preprocessed_frame = preprocess_frame(frame)
-		predictions = model.predict(preprocessed_frame)
+	#def transform(self, frame):
+		#frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+		#preprocessed_frame = preprocess_frame(frame)
+		#predictions = model.predict(preprocessed_frame)
 		
-		if predictions[0][0] < 0.5:
-		    label = "With Mask"
-		else:
-		    label = "Without Mask"
+		#if predictions[0][0] < 0.5:
+		    #label = "With Mask"
+		#else:
+		    #label = "Without Mask"
 		
 		# Overlay the label on the frame
-		cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+		#cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 		
-		return frame
+		#return frame
 
 
 
