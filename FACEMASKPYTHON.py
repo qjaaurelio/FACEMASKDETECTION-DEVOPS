4eef7829f852e1d89a72fd87727f77992c34a7bf
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-16 09:42:59 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -9,11 +9,6 @@ import av
 
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-st.write("""
-# Face(Mask) Detection System
-""")
-
-# Load your model
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model('FacemaskModel')
@@ -21,8 +16,11 @@ def load_model():
 
 model = load_model()
 
-# Preprocessing function
-desired_size = (128, 128)
+st.write("""
+# Face(Mask) Detection System
+""")
+
+desired_size = (128, 128)  # Adjust to your model's input size
 
 def preprocess_frame(frame):
     frame = cv2.resize(frame, desired_size)
@@ -31,29 +29,55 @@ def preprocess_frame(frame):
     return frame
 
 class VideoProcessor:
-    def transform(self, frame):
-        frm = frame.to_ndarray(format="bgr24")
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
 
-        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
-        for x, y, w, h in faces:
-            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            
-            face_roi = frm[y:y+h, x:x+w]
-            preprocessed_face = preprocess_frame(face_roi)
-            
-            predictions = model.predict(preprocessed_face)
-    
-            if predictions[0][0] < 0.5:
-                label = "With Mask"
-            else:
-                label = "Without Mask"
-    
-            cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
+		for x,y,w,h in faces:
+			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+			#label = "With Mask"  # Example label, replace it with your logic
+			#cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-webrtc_ctx = webrtc_streamer(key="key", video_transformer_factory=VideoProcessor,
+		
+        
+	    	#predictions = model.predict(faces)
+	    	#if predictions[0][0] < 0.5:
+	            #label = "With Mask"
+	       # else:
+	       #     label = "Without Mask"
+	    		
+	       # cv2.putText(frm, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	
+		return av.VideoFrame.from_ndarray(frm, format='bgr24')
+		
+	def transform(self, frame):
+	        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+	        preprocessed_frame = preprocess_frame(frame)
+	        predictions = model.predict(preprocessed_frame)
+	
+	        if predictions[0][0] < 0.5:
+	            label = "With Mask"
+	        else:
+	            label = "Without Mask"
+	
+	        # Overlay the label on the frame
+	        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	
+	        return frame
+
+   # def recv(self, frame):
+   #     frm = frame.to_ndarray(format="bgr24")
+
+    #    faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+    #    for x, y, w, h in faces:
+    #        cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    #    return av.VideoFrame.from_ndarray(frm, format='bgr24')
+
+
+webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
                 rtc_configuration=RTCConfiguration(
                     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
                 ))
