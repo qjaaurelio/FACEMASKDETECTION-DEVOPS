2bec6765de6539e49cf2ee9e06729cbd655fc84a
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-17 21:38:40 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -17,7 +17,7 @@ def load_model():
 model = load_model()
 
 st.write("""
-# Face(Mask) Detection System
+# Mask Detection System
 """)
 
 desired_size = (128, 128)  # Adjust to your model's input size
@@ -28,37 +28,34 @@ def preprocess_frame(frame):
     frame = np.expand_dims(frame, axis=0)  # Add batch dimension
     return frame
 
-class VideoProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        preprocessed_frame = preprocess_frame(frame)
+        predictions = model.predict(preprocessed_frame)
 
-		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+        if predictions[0][0] < 0.5:
+            label = "With Mask"
+        else:
+            label = "Without Mask"
 
-		for x,y,w,h in faces:
-			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
-			label = "Without Mask"  # Example label, replace it with your logic
-			cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	
-		return av.VideoFrame.from_ndarray(frm, format='bgr24')
-		
-	def transform(self, frame):
-	        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-	        preprocessed_frame = preprocess_frame(frame)
-	        predictions = model.predict(preprocessed_frame)
-	
-	        if predictions[0][0] < 0.5:
-	            label = "With Mask"
-	        else:
-	            label = "Without Mask"
-	
-	        # Overlay the label on the frame
-	        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-	
-	        return frame
+        # Overlay the label on the frame
+        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        return frame
+
+    def recv(self, frame):
+        frm = frame.to_ndarray(format="bgr24")
+
+        faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+        for x, y, w, h in faces:
+            cv2.rectangle(frm, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
-
-webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+webrtc_ctx = webrtc_streamer(key="key", video_processor_factory=VideoTransformer,
                 rtc_configuration=RTCConfiguration(
                     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
                 ))
