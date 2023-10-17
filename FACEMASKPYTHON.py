c0a49055476b45f3f4c9f2eecaaefa8353bddb28
qjaaurelio <143151915+qjaaurelio@users.noreply.github.com>
2023-10-17 21:34:39 +0800

    Update FACEMASKPYTHON.py

--- FACEMASKPYTHON.py
+++ FACEMASKPYTHON.py
@@ -29,33 +29,32 @@ def preprocess_frame(frame):
     return frame
 
 class VideoProcessor:
-	#def recv(self, frame):
-		#frm = frame.to_ndarray(format="bgr24")
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
+
+		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
+
+		for x,y,w,h in faces:
+			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+			label = "Without Mask"  # Example label, replace it with your logic
+			cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	
-		#faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
-	
-		#for x,y,w,h in faces:
-			#cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
-			#label = "Without Mask"  # Example label, replace it with your logic
-			#cv2.putText(frm, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	
-		#return av.VideoFrame.from_ndarray(frm, format='bgr24')
+		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 		
-	
 	def transform(self, frame):
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-		preprocessed_frame = preprocess_frame(frame)
-		predictions = model.predict(preprocessed_frame)
-		
-		if predictions[0][0] < 0.5:
-		    label = "With Mask"
-		else:
-		    label = "Without Mask"
-		
-		# Overlay the label on the frame
-		cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-		
-		return frame
+	        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+	        preprocessed_frame = preprocess_frame(frame)
+	        predictions = model.predict(preprocessed_frame)
+	
+	        if predictions[0][0] < 0.5:
+	            label = "With Mask"
+	        else:
+	            label = "Without Mask"
+	
+	        # Overlay the label on the frame
+	        cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+	
+	        return frame
 
 
 
